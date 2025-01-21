12decca421ea4689ae5dccb056888d8a1aa07b1c
MaybeSemigroup <higeagito@yahoo.co.jp>
2025-01-22 00:45:11 +0900

    Fix malformed pose id. Add Pose control to UI. Add release build task and restructure repository.

--- CCPoseLoader.cs
+++ CCPoseLoader.cs
@@ -1,5 +1,6 @@
-﻿using HarmonyLib;
+using HarmonyLib;
 using BepInEx;
+using BepInEx.Logging;
 using BepInEx.Configuration;
 using BepInEx.Unity.IL2CPP;
 using Il2CppInterop.Runtime;
@@ -23,6 +24,8 @@ namespace CCPoseLoader
     // abdata/ui.unity3d should be loaded before.
     internal class UIRefs
     {
+        // pose control object
+        internal GameObject PoseControl;
         // window background
         internal Image Window;
         // outlined text component
@@ -31,12 +34,14 @@ namespace CCPoseLoader
         internal Image Image;
         // per state button image
         internal Button Button;
-        internal UIRefs(AssetBundle bundle) :
+
+        internal UIRefs(AssetBundle bundle, GameObject poseControl) :
             this(new GameObject(bundle.LoadAsset_Internal("assets/illgames/assetbundle/custom/ui/03_clothes/00_clothes.prefab", Il2CppType.From(typeof(GameObject))).Pointer))
-        { }
+        {
+            PoseControl = poseControl;
+        }
         internal UIRefs(GameObject refui) : this(refui.transform.Find("BaseView"))
         { }
-
         internal UIRefs(Transform refui) :
             this(refui.Find("BG").GetComponent<Image>(), refui.Find("Contents"))
         { }
@@ -67,6 +72,7 @@ namespace CCPoseLoader
     // abstraction of labeled (categorized) status control
     internal class StateControl<TLabels, TStatus>
     {
+        internal string Name;
         // internal label value to display representation
         internal Func<TLabels, string> TranslateLabel;
         // internal status value to display representation
@@ -85,6 +91,33 @@ namespace CCPoseLoader
             Toggle(label);
             ui.SetText(TranslateState(Get(label)));
         };
+        internal GameObject ComposeControl(GameObject go, UIRefs refs)
+        {
+            go = go.Wrap(new GameObject(Name)).HorizontalLayout();
+            ComposeControl(
+                VerticalLayout(go.Wrap(new GameObject("Labels"))),
+                VerticalLayout(go.Wrap(new GameObject("Buttons"))),
+                VerticalLayout(go.Wrap(new GameObject("Status"))), refs);
+            return go;
+        }
+        internal GameObject VerticalLayout(GameObject go)
+        {
+            go.AddComponent<RectTransform>();
+            go.AddComponent<LayoutElement>().preferredHeight = 25 * Labels.Count();
+            go.AddComponent<VerticalLayoutGroup>().With(ui =>
+            {
+                ui.spacing = 1;
+                ui.childAlignment = TextAnchor.LowerCenter;
+                ui.childControlWidth = true;
+                ui.childControlHeight = true;
+            });
+            go.AddComponent<ContentSizeFitter>().With(ui =>
+            {
+                ui.verticalFit = ContentSizeFitter.FitMode.PreferredSize;
+                ui.horizontalFit = ContentSizeFitter.FitMode.PreferredSize;
+            });
+            return go;
+        }
         // compose each label to {labels, buttons, status} control
         internal void ComposeControl(GameObject labels, GameObject buttons, GameObject status, UIRefs refs) =>
             Labels.Do(label =>
@@ -95,12 +128,31 @@ namespace CCPoseLoader
                         ComposeAction(label, status.Wrap(new GameObject("Label"))
                             .State(refs, TranslateState(Get(label))).GetComponent<TextMeshProUGUI>()));
             });
+        internal void UpdateState()
+        {
+            if (HumanCustom.Instance.transform.Find("UI").Find("Root").Find(Plugin.Name) != null)
+            {
+                UpdateState(HumanCustom.Instance.transform.Find("UI")
+                    .Find("Root").Find(Plugin.Name).Find("Window").Find(Name).Find("Status")
+                    .GetComponentsInChildren<TextMeshProUGUI>());
+            }
+        }
+        internal void UpdateState(TextMeshProUGUI[] status)
+        {
+            for (int index = Labels.Count(); index >= 0; --index)
+            {
+                Enumerable.Range(0, Labels.Count()).Zip(Labels)
+                    .Do(tuple => status[tuple.Item1].SetText(TranslateState(Get(tuple.Item2))));
+            }
+        }
     }
     // UI generation helpers
     internal static class UIFactory
     {
+        internal static ConfigEntry<bool> InitialVisibility { get; set; }
         // tweaks to make local namespace clean
         internal static void With<T>(this T item, Action<T> action) => action(item);
+        internal static void With<T, S>(this T item, Func<T, S> action) => action(item);
         // tweaks to link game object hierarchy first
         internal static GameObject Wrap(this GameObject outer, GameObject inner)
         {
@@ -142,11 +194,30 @@ namespace CCPoseLoader
             dest.spriteState = refs.spriteState;
             return dest;
         }
+        internal static GameObject HorizontalLayout(this GameObject go) {
+            go.AddComponent<RectTransform>();
+            go.AddComponent<LayoutElement>();
+            go.AddComponent<HorizontalLayoutGroup>().With(ui =>
+            {
+                ui.padding = new(0, 0, 10, 10);
+                ui.spacing = 5;
+                ui.childAlignment = TextAnchor.MiddleCenter;
+                ui.childControlWidth = true;
+                ui.childControlHeight = true;
+            });
+            go.AddComponent<ContentSizeFitter>().With(ui =>
+            {
+                ui.verticalFit = ContentSizeFitter.FitMode.PreferredSize;
+                ui.horizontalFit = ContentSizeFitter.FitMode.PreferredSize;
+            });
+            return go;
+        }
         // add button to open clothes and accessory status window
         internal static void UI(this GameObject go, UIRefs refs)
         {
             go.AddComponent<RectTransform>();
-            go.AddComponent<LayoutElement>().With(ui => {
+            go.AddComponent<LayoutElement>().With(ui =>
+            {
                 ui.minWidth = 64;
                 ui.minHeight = 64;
             });
@@ -168,6 +239,7 @@ namespace CCPoseLoader
             go.AddComponent<GraphicRaycaster>();
             go.GetComponent<RectTransform>();
             go.Wrap(new GameObject("Window")).Window(refs);
+            go.SetActive(InitialVisibility.Value);
             return () => { go.SetActive(!go.active); };
         }
         // internal pose index to display representation
@@ -211,56 +283,15 @@ namespace CCPoseLoader
                     ui.SetText(TranslatePose(HumanCustom.Instance?.NowPose ?? 0));
                 });
             });
-            go.Wrap(new GameObject("ClothesControl")).Control(refs, ClothesControl);
-            go.Wrap(new GameObject("AccessoryControl")).Control(refs, AccessoryControl);
+            go.Wrap(refs.PoseControl);
+            ClothesControl.ComposeControl(go, refs);
+            AccessoryControl.ComposeControl(go, refs);
             return go;
         }
         // clothes status control
         internal static StateControl<ChaFileDefine.ClothesKind, ChaFileDefine.ClothesState> ClothesControl;
         // accessory status control
         internal static StateControl<int, bool> AccessoryControl;
-        // create each {label, button, state} control component
-        internal static GameObject Control<TLabels, TStatus>(this GameObject go, UIRefs refs, StateControl<TLabels, TStatus> control)
-        {
-            go.AddComponent<RectTransform>();
-            go.AddComponent<LayoutElement>();
-            go.AddComponent<HorizontalLayoutGroup>().With(ui =>
-            {
-                ui.padding = new(0, 0, 10, 10);
-                ui.spacing = 5;
-                ui.childAlignment = TextAnchor.MiddleCenter;
-                ui.childControlWidth = true;
-                ui.childControlHeight = true;
-            });
-            go.AddComponent<ContentSizeFitter>().With(ui =>
-            {
-                ui.verticalFit = ContentSizeFitter.FitMode.PreferredSize;
-                ui.horizontalFit = ContentSizeFitter.FitMode.PreferredSize;
-            });
-            control.ComposeControl(
-                go.Wrap(new GameObject("Labels")).VerticalLayout(control),
-                go.Wrap(new GameObject("Buttons").VerticalLayout(control)),
-                go.Wrap(new GameObject("Status").VerticalLayout(control)), refs);
-            return go;
-        }
-        internal static GameObject VerticalLayout<TLabels, TStatus>(this GameObject go, StateControl<TLabels, TStatus> control)
-        {
-            go.AddComponent<RectTransform>();
-            go.AddComponent<LayoutElement>().preferredHeight = 25 * control.Labels.Count();
-            go.AddComponent<VerticalLayoutGroup>().With(ui =>
-            {
-                ui.spacing = 1;
-                ui.childAlignment = TextAnchor.LowerCenter;
-                ui.childControlWidth = true;
-                ui.childControlHeight = true;
-            });
-            go.AddComponent<ContentSizeFitter>().With(ui =>
-            {
-                ui.verticalFit = ContentSizeFitter.FitMode.PreferredSize;
-                ui.horizontalFit = ContentSizeFitter.FitMode.PreferredSize;
-            });
-            return go;
-        }
         // create label component
         internal static GameObject Label(this GameObject go, UIRefs refs, string label)
         {
@@ -302,6 +333,30 @@ namespace CCPoseLoader
             });
             return go;
         }
+        internal static GameObject PoseControl(this Transform layout, GameObject go) {
+            go.HorizontalLayout();
+            go.Wrap(layout.Find("ptnSelect").Find("btnPrev").gameObject.MirrorButton());
+            go.Wrap(layout.Find("ptnSelect").Find("btnNext").gameObject.MirrorButton());
+            go.Wrap( layout.Find("tglPose").gameObject.MirrorToggle());
+            return go;
+        }
+        internal static GameObject MirrorButton(this GameObject go) {
+            var mirror = UnityEngine.Object.Instantiate(go);
+            mirror.GetComponent<Button>()
+                .onClick.AddListener(go.GetComponent<Button>().MirrorAction());
+            return mirror;
+        }
+        internal static GameObject MirrorToggle(this GameObject go) {
+            var mirror = UnityEngine.Object.Instantiate(go);
+            mirror.GetComponent<Toggle>()
+                .onValueChanged.AddListener(go.GetComponent<Toggle>().MirrorAction());
+            return mirror;
+        }
+        internal static Action MirrorAction(this Button ui) =>
+            () => ui.onClick.Invoke();
+        internal static Action<bool> MirrorAction(this Toggle ui) =>
+            (value) => ui.onValueChanged.Invoke(value);
+
     }
     internal static class Extension
     {
@@ -329,6 +384,15 @@ namespace CCPoseLoader
                         .SetText(UIFactory.TranslatePose(HumanCustom.Instance?.NowPose ?? 0));
             }
         }
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(HumanCustom), nameof(HumanCustom.UpdateClothesState))]
+        [HarmonyPatch(typeof(HumanCustom), nameof(HumanCustom.SetClothesState), typeof(HumanCustom.ClothStateSeter.State))]
+        internal static void UpdateClothesStatePostfix() =>
+            UIFactory.ClothesControl.UpdateState();
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(HumanCustom), nameof(HumanCustom.UpdateAccessoryState))]
+        internal static void UpdateAccessoryStatePostfix() =>
+            UIFactory.AccessoryControl.UpdateState();
         // UI should be made after abdata/ui.unity3d loaded
         // so there would be better timing ...
         [HarmonyPostfix]
@@ -340,22 +404,22 @@ namespace CCPoseLoader
                 if (Path.GetFileName(bundle.name).Equals("ui.unity3d"))
                 {
                     disposableComponent.transform.Find("Window").Find("StateWindow")
-                        .Find("Pose").Find("PosePtn").Find("Layout").With(layout =>
+                        .Find("Pose").Find("PosePtn").Find("Layout").With(layout =>{
+                        // tweaks to mini state window
+                        layout.Find("ptnSelect").Find("InputField_Integer").With(input =>
                         {
-                            // tweaks to mini state window
-                            layout.Find("ptnSelect").Find("InputField_Integer").With(input =>
-                            {
-                                input.GetComponent<RectTransform>().sizeDelta = new(80, 26);
-                                input.GetComponent<TMP_InputField>().characterLimit = 5;
-                            });
-                            layout.gameObject.Wrap(new GameObject("ShowHide")).UI(new UIRefs(bundle));
+                            input.GetComponent<RectTransform>().sizeDelta = new(80, 26);
+                            input.GetComponent<TMP_InputField>().characterLimit = 5;
                         });
+                        layout.gameObject.Wrap(new GameObject("ShowHide"))
+                            .UI(new UIRefs(bundle, layout.PoseControl(new GameObject("PoseControl"))));
+                    });
                     return;
                 }
             }
         }
         internal static string QualifiedName(this PoseInfoData info) =>
-            $"{info.Bundle}:${info.Asset}:${info.State}";
+            $"{info.Bundle}:{info.Asset}:{info.State}";
         internal static string ConfigPath(this string value) =>
             Path.Combine(Paths.ConfigPath, Plugin.Name, value);
         internal static Func<string, string> GuiTranslation(this Dictionary<string, string> names) =>
@@ -379,6 +443,7 @@ namespace CCPoseLoader
         private Func<string, string> PoseTranslation;
         public override void Load()
         {
+            UIFactory.InitialVisibility = Config.Bind(new ConfigDefinition("General", "InitialVisibility"), true);
             Poses = Config.Bind(new ConfigDefinition("General", "PoseFile"), "poses.json");
             Translations = Config.Bind(new ConfigDefinition("General", "TranslationDir"), "ja_JP");
 
@@ -395,14 +460,16 @@ namespace CCPoseLoader
             };
             UIFactory.ClothesControl = new StateControl<ChaFileDefine.ClothesKind, ChaFileDefine.ClothesState>
             {
+                Name = "ClothesCOntrol",
                 TranslateLabel = (input) => GuiTranslation(Enum.GetName(input)),
                 TranslateState = (input) => GuiTranslation(Enum.GetName(input)),
                 Toggle = (input) => HumanCustom.Instance?.Human?.cloth?.SetClothesStateNext(input),
                 Get = (input) => HumanCustom.Instance?.Human?.cloth?.GetClothesStateType(input) ?? ChaFileDefine.ClothesState.Naked,
                 Labels = Enum.GetValues<ChaFileDefine.ClothesKind>()
             };
-            UIFactory.AccessoryControl = new StateControl<int, bool>
+            UIFactory.AccessoryControl = new StateControl<int, bool>()
             {
+                Name = "AccessoryControl",
                 TranslateLabel = (input) => $"{GuiTranslation("Slot")}{input + 1}",
                 TranslateState = (input) => GuiTranslation(input ? "ON" : "OFF"),
                 Toggle = (input) => HumanCustom.Instance?.Human?.acs?.SetAccessoryState(input, !HumanCustom.Instance?.Human?.acs?.IsAccessory(input) ?? false),
--- README.md
+++ README.md
@@ -1,8 +1,20 @@
 # SVS-CCPoseLoader
-additional pose loader for SamabakeScramble Character creation
+
+Additional pose loader for SamabakeScramble Character creation
 
 # Prerequests
 
  * BepInEx v6
  * BepInEx.ConfigurationManager
  
+# Installation
+
+Extract the release to game root.
+
+# How to use
+
+Start character creation then you'll see addtional menu.
+ 
+Poses to load are defined in .json format, you can made your own in relative path from ```(game root)/BepInEx/config/CCPoseLoader```.
+
+There are predefined (and not well tested) list packaged in release.
