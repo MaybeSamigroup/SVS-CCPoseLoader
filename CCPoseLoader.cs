c882c3c18d722a927f45ef4244f08a496e20c944
MaybeSemigroup <higeagito@yahoo.co.jp>
2026-01-07 01:15:17 +0900

    Restructure MSBuilds and adapt to CoastalSmell/Fishbone changes.

--- CCPoseLoader.cs
+++ CCPoseLoader.cs
@@ -3,6 +3,10 @@ using System.IO;
 using System.Linq;
 using System.Text.Json;
 using System.Collections.Generic;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Reactive.Disposables;
 using HarmonyLib;
 using BepInEx;
 using BepInEx.Configuration;
@@ -15,21 +19,22 @@ using MotionIK = ILLGAMES.Rigging.MotionIK;
 using MotionIK = ILLGames.Rigging.MotionIK;
 #endif
 using CoastalSmell;
-using PoseBuff = Il2CppSystem.Collections.Generic.List<CharacterCreation.ListInfo.PoseInfoData>;
-using PoseDefs = Il2CppSystem.Collections.Generic.IReadOnlyList<CharacterCreation.ListInfo.PoseInfoData>;
 using PoseSet = System.Collections.Generic.Dictionary<string, System.Collections.Generic.Dictionary<string, string[]>>;
 
 namespace CCPoseLoader
 {
     public static class Event
     {
-        public static List<string> PoseIds = new ();
-        public static string QualifiedName(this PoseInfoData info) =>
-            $"{info.Bundle}:{info.Asset}:{info.State}";
-        internal static ConfigEntry<string> MalePoses { get; set; }
-        internal static ConfigEntry<string> FemalePoses { get; set; } 
-        static string ConfigPath(this string value) => Path.Combine(Paths.GameRootPath, "UserData", "plugins", Plugin.Name, value);
+        static ConfigEntry<string> MalePoses = 
+            Plugin.Instance.Config.Bind(new ConfigDefinition("General", "MalePoseFile"), "male/poses.json");
+
+        internal static ConfigEntry<string> FemalePoses =
+            Plugin.Instance.Config.Bind(new ConfigDefinition("General", "FemalePoseFile"), "female/poses.json");
+
+        static string ConfigPath(this string value) => Path.Combine(Util.UserDataPath, "plugins", Plugin.Name, value);
+
         static string CurrentConfig(HumanCustom custom) => ConfigPath(custom.IsMale() ? MalePoses.Value : FemalePoses.Value);
+
         static IEnumerable<PoseInfoData> CurrentPoses(HumanCustom custom) =>
             JsonSerializer.Deserialize<PoseSet>(File.OpenRead(CurrentConfig(custom)))
                 .SelectMany(bundle2assets => bundle2assets.Value
@@ -39,36 +44,38 @@ namespace CCPoseLoader
                     Asset = asset2states.Key,
                     State = state
                 })));
-        static Action<PoseInfoData> EntryPoseId = info => PoseIds.Add(info.QualifiedName());
-        static PoseBuff EntryPoses(this PoseBuff dst, IEnumerable<PoseInfoData> src) =>
-            dst.With(() => src.ForEach(EntryPoseId + dst.Add));
-        static bool ReadyPoses() =>
-            HumanCustom.Instance.PoseList != null; 
-        static void ApplyPoses() =>
-            HumanCustom.Instance.PoseList = new PoseDefs(new PoseBuff()
-                .EntryPoses(HumanCustom.Instance.PoseList.Yield())
-                .EntryPoses(CurrentPoses(HumanCustom.Instance)).Pointer);
-        internal static void Initialize() =>
-            Util<HumanCustom>.Hook(OnEnterCustom, OnLeaveCustom);
+
+        static bool CheckPoses(HumanCustom scene) => scene.PoseList != null;
+
+        static void ApplyPoses(HumanCustom scene) =>
+            scene.PoseList = new(scene.PoseList.Yield().Concat(CurrentPoses(scene)).AsIl2Cpp().Pointer);
+
         static void FixBinder(MotionIKDataBinder binder) =>
             (binder != null && binder.Data != null && binder.MotionIK.Data == null)
                 .Maybe(() => binder.MotionIK.SetData(binder.Data));
-        static void FixBinder() =>
-            FixBinder(HumanCustom.Instance.Human?.gameObject?.GetComponent<MotionIKDataBinder>());
-        static void OnEnterCustom()
-        {
-            Hooks.MotionIKLoadData += FixBinder;
-            Util.DoOnCondition(ReadyPoses, ApplyPoses);
-        }
-        static void OnLeaveCustom()
-        {
-            Hooks.MotionIKLoadData -= FixBinder;
-            PoseIds.Clear();
-        }
+
+        static CompositeDisposable Subscriptions;
+
+        internal static void Subscribe(HumanCustom scene) => Subscriptions = [
+            scene.OnUpdateAsObservable()
+                .Select(_ => scene)
+                .FirstAsync(CheckPoses)
+                .Subscribe(ApplyPoses),
+            Hooks.OnMotionIKLoadData
+                .Select(_ => scene?.Human?.gameObject?.GetComponent<MotionIKDataBinder>())
+                .Subscribe(FixBinder)
+        ];
+
+        internal static IDisposable[] Initialize() => [
+            SingletonInitializerExtension<HumanCustom>.OnStartup.Subscribe(Subscribe),
+            SingletonInitializerExtension<HumanCustom>.OnDestroy.Subscribe(_ => Subscriptions.Dispose()),
+        ];
+                
     }
     internal static class Hooks
     {
-        internal static event Action MotionIKLoadData = delegate { };
+        static Subject<Unit> MotionIKLoadData = new();
+        internal static IObservable<Unit> OnMotionIKLoadData => MotionIKLoadData.AsObservable();
 
         [HarmonyPostfix]
         [HarmonyPatch(typeof(StateMiniSelection), nameof(StateMiniSelection.InitPosePack))]
@@ -77,7 +84,10 @@ namespace CCPoseLoader
 
         [HarmonyPostfix]
         [HarmonyPatch(typeof(MotionIK), nameof(MotionIK.LoadData))]
-        static void MotionIKLoadDataPostfix() => MotionIKLoadData();
+        static void MotionIKLoadDataPostfix() => MotionIKLoadData.OnNext(Unit.Default);
+
+        internal static IDisposable Initialize() =>
+            Disposable.Create(Harmony.CreateAndPatchAll(typeof(Hooks), $"{Plugin.Name}.Hooks").UnpatchSelf);
     }
 
     [BepInProcess(Process)]
@@ -88,16 +98,11 @@ namespace CCPoseLoader
         internal static Plugin Instance;
         public const string Name = "CCPoseLoader";
         public const string Guid = $"{Process}.{Name}";
-        public const string Version = "2.1.1";
-        private Harmony Patch;
-        public override void Load()
-        {
-            Patch = Harmony.CreateAndPatchAll(typeof(Hooks), $"{Name}.Hooks");
-            Instance = this;
-            Event.MalePoses = Config.Bind(new ConfigDefinition("General", "MalePoseFile"), "male/poses.json");
-            Event.FemalePoses = Config.Bind(new ConfigDefinition("General", "FemalePoseFile"), "female/poses.json");
-            Event.Initialize();
-        }
-        public override bool Unload() => true.With(Patch.UnpatchSelf) && base.Unload();
+        public const string Version = "2.2.0";
+        CompositeDisposable Subscriptions;
+        public Plugin() : base() => Instance = this;
+        public override void Load() => Subscriptions = [Hooks.Initialize(), ..Event.Initialize()];
+        public override bool Unload() =>
+            true.With(Subscriptions.Dispose) && base.Unload();
     }
 }
